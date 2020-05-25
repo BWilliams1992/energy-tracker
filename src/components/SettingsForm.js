c02dfa7457898e68abbafde23f9c92d10f0a64d7
Ben Williams <bwilliams0001992@gmail.com>
2020-05-25 16:01:10 +0100

    added more styling, started adding option to track gas and electricity

--- src/components/SettingsForm.js
+++ src/components/SettingsForm.js
@@ -4,6 +4,7 @@ export default class SettingsForm extends React.Component {
   constructor(props) {
     super(props)
     this.state={
+      gas: props.settings ? props.settings.gas : "false",
       dayRate: props.settings ? props.settings.dayRate : 0,
       nightRate:props.settings ? props.settings.nightRate : 0,
       standingCharge:props.settings ? props.settings.standingCharge : 0,
@@ -30,6 +31,10 @@ export default class SettingsForm extends React.Component {
     this.setState(() => ({ standingCharge })) 
   }
 }
+  onGasChange = (e) => {
+    const gas = e.target.value
+    this.setState(() => ({ gas }))
+  }
 
   onSubmit = (e) => {
   e.preventDefault()
@@ -38,6 +43,7 @@ export default class SettingsForm extends React.Component {
   } else {
     this.setState(() => ({ error: '' }))
     this.props.onSubmit({
+      gas:this.state.gas,
       dayRate:parseFloat(this.state.dayRate),
       nightRate: parseFloat(this.state.nightRate),
       standingCharge: parseFloat(this.state.standingCharge)
@@ -50,12 +56,18 @@ export default class SettingsForm extends React.Component {
       <h3 className="settings__title">Settings:</h3>
       <form id="settings-form" onSubmit={this.onSubmit}>
         {this.state.error && <p>{this.state.error}</p>}
+        <p>
+          <label htmlFor="electric">Electricity</label>
+          <input type="radio" id="electric" value="false" name="gas" onChange={this.onGasChange} checked={this.state.gas === "false"} />
+          <label htmlFor="gas-and-electric">Gas & Electricity</label>
+          <input type="radio" id="gas-and-electric" value="true" name="gas" onChange={this.onGasChange} checked={this.state.gas === "true"} />
+        </p>
         <label htmlFor="day-rate">Day rate:</label>
-        <input type="text" name="day-rate" value={this.state.dayRate} onChange={this.onDayRateChange} />
+        <input type="text" id="day-rate" value={this.state.dayRate} onChange={this.onDayRateChange} />
         <label htmlFor="night-rate">Night rate:</label>
-        <input type="text" name="night-rate" value={this.state.nightRate} onChange={this.onNightRateChange} />
+        <input type="text" id="night-rate" value={this.state.nightRate} onChange={this.onNightRateChange} />
         <label htmlFor="standing-charge">Standing charge:</label>
-        <input type="text" name="standing-charge" value={this.state.standingCharge} onChange={this.onStandingChargeChange} />
+        <input type="text" id="standing-charge" value={this.state.standingCharge} onChange={this.onStandingChargeChange} />
         <button>Save Settings</button> 
       </form>
     </div>
